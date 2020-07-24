ae2b4d49eb562bdd41fd34cfac722088b466b0f7
Evgenii Dolgov <river100@mail.ru>
2020-07-24 11:14:30 +0300

    initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+#Hack and slash game project for self education
+
+Java 11 + OpenGl
---
+++ src/main/java/amaralus/apps/hackandslash/MainApplication.java
@@ -0,0 +1,8 @@
+package amaralus.apps.hackandslash;
+
+public class MainApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
