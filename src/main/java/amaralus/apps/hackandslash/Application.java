1599dcde2beebf61d422cdeb5bfe2e32dbcf115c
Evgenii Dolgov <river100@mail.ru>
2020-07-24 21:58:36 +0300

    Добавлена отрисовка треугольника

--- src/main/java/amaralus/apps/hackandslash/Application.java
+++ src/main/java/amaralus/apps/hackandslash/Application.java
@@ -1,5 +1,6 @@
 package amaralus.apps.hackandslash;
 
+import amaralus.apps.hackandslash.services.io.FileService;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.glfw.GLFWVidMode;
 import org.lwjgl.opengl.GL;
@@ -12,6 +13,8 @@ import java.nio.*;
 import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.*;
 import static org.lwjgl.system.MemoryStack.stackPush;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
@@ -20,6 +23,7 @@ public class Application {
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     private long windowHandle;
+    private FileService fileService = new FileService();
 
     public static void main(String[] args) {
         new Application().run();
@@ -27,7 +31,6 @@ public class Application {
 
     public void run() {
         log.info("Запуск Hack and Slash");
-
 
         try (var errorCallback = GLFWErrorCallback.createPrint(System.err).set()) {
             init();
@@ -82,14 +85,68 @@ public class Application {
     private void loop() {
         GL.createCapabilities();
 
-        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        int vertexShader = loadShader(GL_VERTEX_SHADER, "vertex");
+        int fragmentShader = loadShader(GL_FRAGMENT_SHADER, "fragment");
+
+        int shaderProgram = glCreateProgram();
+        glAttachShader(shaderProgram, vertexShader);
+        glAttachShader(shaderProgram, fragmentShader);
+        glLinkProgram(shaderProgram);
+
+        log.debug("shader program log: {}", glGetProgramInfoLog(shaderProgram));
+
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+
+        float[] vertices = {
+                -0.5f, -0.5f, 0.0f,
+                0.5f, -0.5f, 0.0f,
+                0.0f,  0.5f, 0.0f
+        };
+
+        int vao = glGenVertexArrays();
+        int vbo = glGenBuffers();
+
+        glBindVertexArray(vao);
+
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * Float.BYTES, 0L);
+        glEnableVertexAttribArray(0);
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
 
         while (!glfwWindowShouldClose(windowHandle)) {
+            glfwPollEvents();
+
+            glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-            glfwSwapBuffers(windowHandle);
+            glUseProgram(shaderProgram);
 
-            glfwPollEvents();
+            glBindVertexArray(vao);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+            glBindVertexArray(0);
+
+            glfwSwapBuffers(windowHandle);
         }
+
+        glDeleteVertexArrays(vao);
+        glDeleteBuffers(vbo);
+    }
+
+    private int loadShader(int type, String name) {
+        String fileName = "shaders/" + name + ".glsl";
+        int shader = glCreateShader(type);
+
+        glShaderSource(shader, fileService.loadFileAsStringFromResources(fileName));
+        glCompileShader(shader);
+
+        String compileLog = glGetShaderInfoLog(shader);
+        log.debug("Результат компиляции шейдера {}: {}", name, compileLog.isEmpty() ? "compilation successful" : compileLog);
+
+        return shader;
     }
 }
