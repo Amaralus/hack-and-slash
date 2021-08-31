2283ac245368ba40d8307421299626dc7b2a5d7d
Evgenii Dolgov <river100@mail.ru>
2021-08-31 10:34:43 +0300

    Версия обновлена до 0.2.4

--- src/main/java/amaralus/apps/hackandslash/config/ApplicationConfig.java
+++ src/main/java/amaralus/apps/hackandslash/config/ApplicationConfig.java
@@ -16,7 +16,7 @@ public class ApplicationConfig {
 
     @Bean
     public Window window(WindowProperties properties) {
-        var window = new Window(properties.getWidth(), properties.getHeight(), properties.getTitle() + " v-0.2.3");
+        var window = new Window(properties.getWidth(), properties.getHeight(), properties.getTitle() + " v-0.2.4");
         window.show();
         return window;
     }
